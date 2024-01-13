454fb90005a5f3f1feb9f5f944f75ea694b0ffcc
Harish Kurma <hkurma0504@gmail.com>
2024-01-13 05:09:01 -0500

    Fix accidental change

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,6 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
-import Link from 'next/link';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -44,7 +43,6 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
-    <Link></Link>;
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
